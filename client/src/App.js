b91d3df4ee8e1aa65516c19e06626490a6632c10
Know167 <191200116035@asoit.edu.in>
2023-12-31 17:40:02 -0500

    Initial commit with server setup, schema and models and an empty new client folder

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Header from "./components/Header";
+import Clients from "./components/Clients";
+
+const cache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                clients: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    },
+                },
+                projects: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    },
+                },
+            },
+        },
+    },
+});
+
+const client = new ApolloClient({
+    uri: "http://localhost:5000/graphql",
+    cache,
+});
+
+function App() {
+    return (
+        <>
+            <ApolloProvider client={client}>
+                <div className="container">
+                    <Header />
+                    <Clients />
+                </div>
+            </ApolloProvider>
+        </>
+    );
+}
+
+export default App;
