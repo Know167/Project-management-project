84c5f9200490d8ee4d060921af26b39c59684135
Know167 <191200116035@asoit.edu.in>
2024-01-05 14:43:45 -0500

    minor changes in backend
    Changed the schema to find the project by Id instead of using just the find method. Also passing the id as a variable.

--- client/src/components/ProjectInfo.jsx
+++ client/src/components/ProjectInfo.jsx
@@ -7,7 +7,7 @@ const ProjectInfo = () => {
     const { id } = useParams();
     console.log(id);
     const { loading, error, data } = useQuery(GET_PROJECT, {
-        variables: { id: {id} },
+        variables: { id },
     });
     if (loading) <Spinner />;
     if (error) <p>Something Somewhere Went Sideways</p>;
