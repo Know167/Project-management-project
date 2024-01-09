b329186ae593793a0b89a802858c58a4a9b86ff0
Know167 <191200116035@asoit.edu.in>
2024-01-08 19:48:08 -0500

    Updated the Home page
    Minor changes for the update page to make it more appealing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,27 @@
-const Home = () => {
-  return (
-    <div>
-      Welcome to Project Manager
-    </div>
-  )
-}
+import { Link } from "react-router-dom"; 
 
-export default Home
+const Home = () => {
+    return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 text-center">
+                    <h1>Welcome to Your Project Management App</h1>
+                    <p className="lead">
+                        Manage your projects efficiently and stay organized with
+                        our app.
+                    </p>
+                    <div className="mt-4">
+                        <Link to="/projects" className="btn btn-blue me-2 text-white shadow-sm">
+                            View Projects
+                        </Link>
+                        <Link to="/add-project" className="btn btn-success shadow-sm">
+                            Add New Project
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
