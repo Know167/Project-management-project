be076a35c8f5a13ce5889bc6f3e574211e12c780
Know167 <191200116035@asoit.edu.in>
2024-02-15 17:18:06 -0500

    Minor server configuration changes for deployment

--- server/api/index.js
+++ server/api/index.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
-const schema = require("./schema/schema");
-const connectDB = require("./config/db");
+const schema = require("../schema/schema");
+const connectDB = require("../config/db");
 
 const port = process.env.PORT || 5000;
 const app = express();
