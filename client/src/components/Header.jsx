0ec7eccb0921091420bdbc74897df3a29f6861c7
Know167 <191200116035@asoit.edu.in>
2024-02-24 16:02:14 -0500

    Gave navigation a new look with Ant-design

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,40 +1,78 @@
-import logo from './assets/1.png'
+import { useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
+import logo from "./assets/1.png";
+import { Menu } from "antd";
+import PersonIcon from "@mui/icons-material/Person";
+import {
+    AppstoreOutlined,
+    MailOutlined,
+    SettingOutlined,
+} from "@ant-design/icons";
 
 const Header = () => {
-  return (
-      <div className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a
-              href="/"
-              className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-              <img
-                  src={logo}
-                  className="image-fluid"
-                  alt="logo"
-                  width={"60px"}
-                  height={"60px"}
-              />
-              <span className="fs-3 fw-medium ms-4">Verity Manage</span>
-          </a>
+    const navigate = useNavigate();
 
-          <ul className="nav nav-pills">
-              <li className="nav-item">
-                  <a
-                      href="/clients"
-                      className="btn btn-lg border fw-medium text-dark me-2"
-                      aria-current="page">
-                      Clients
-                  </a>
-              </li>
-              <li className="nav-item">
-                  <a
-                      href="/projects"
-                      className="btn btn-lg border nav-link fw-medium text-dark">
-                      Projects
-                  </a>
-              </li>
-          </ul>
-      </div>
-  );
-}
+    const items = [
+        {
+            label: "Clients",
+            key: "clients",
+            icon: <PersonIcon/>,
+        },
+        {
+            label: "Projects",
+            key: "projects",
+            icon: <AppstoreOutlined />,
+        },
+    ];
+    const [current, setCurrent] = useState("");
+    const onClick = (e) => {
+        console.log(e.key);
+        navigate(`/${e.key}`);
+        setCurrent(e.key);
+    };
+    return (
+        <>
+            <div className="justify-content-center mb-3 border-bottom">
+                <a
+                    href="/"
+                    className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                    <img
+                        src={logo}
+                        className="image-fluid"
+                        alt="logo"
+                        width={"60px"}
+                        height={"60px"}
+                    />
+                    <span className="fs-3 fw-medium ms-4">Verity Manage</span>
+                </a>
+                <Menu
+                    onClick={onClick}
+                    selectedKeys={[current]}
+                    mode="horizontal"
+                    items={items}
+                />
 
-export default Header
+                {/* <ul className="nav nav-pills">
+                <li className="nav-item">
+                    <a
+                        href="/clients"
+                        className="btn btn-lg border fw-medium text-dark me-2"
+                        aria-current="page">
+                        Clients
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a
+                        href="/projects"
+                        className="btn btn-lg border nav-link fw-medium text-dark">
+                        Projects
+                    </a>
+                </li>
+            </ul> */}
+            </div>
+            <Outlet />
+        </>
+    );
+};
+
+export default Header;
